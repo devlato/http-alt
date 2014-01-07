afcdd23974e07819201fcb4d33c821afb894820c
James Halliday <mail@substack.net>
2014-01-07 11:13:46 -0800

    memory usage

--- example/server.js
+++ example/server.js
@@ -3,3 +3,11 @@ var server = http.createServer(function (req, res) {
     res.end('whatever\n');
 });
 server.listen(5000);
+
+if (process.argv[3] === 'mem') {
+    var sprintf = require('sprintf');
+    setInterval(function () {
+        var used = process.memoryUsage().heapUsed;
+        console.log(sprintf('%.2f M', used / 1024 / 1024));
+    }, 1000);
+}
